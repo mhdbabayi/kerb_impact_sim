7035dfda064fed479e35f95fd5d3dc1ade3fda1b
mhdbabayi <m.babayi-semiromi@cranfield.ac.uk>
2023-06-26 12:51:11 +0100

    added to graphs script

--- scripts/graphs.m
+++ scripts/graphs.m
@@ -5,14 +5,14 @@ clear
 %% constants
 R= 0.788/2;
 R_rim = 0.508/2;
-rho = R*0.1;
+rho = R*0.5;
 penetration = 0.03;
 D = R + rho - penetration;
 theta0 = deg2rad(-45);
 base_theta = deg2rad((0:360));
 Ot = [D*cos(theta0) , D*sin(theta0)];
 max_theta_p = acosd(-(rho^2 - (R^2 + D^2))/(2*R*D));
-beta = 2.3;
+beta = 1.3;
 %% symbolics
 clc
 syms theta_s A_s B_s phi theta_p
@@ -33,7 +33,7 @@ A_f = d1_epsilon0_f(theta_f)/beta + epsilon0;
 beam_d2 = d2_beam_f(A_f);
 epsilon0_d2 = d2_epsilon0_f(theta_f);
 plot(rad2deg(theta_f) , beam_d2);
-hold on 
+hold on
 plot(rad2deg(theta_f) , epsilon0_d2)
 separation_angle = theta_f(find(beam_d2 > epsilon0_d2 , 1 , 'first'));
 if isempty(separation_angle) || separation_angle > rad2deg(max_theta_p)
@@ -42,25 +42,47 @@ end
 R_separation = R - epsilon0_f(separation_angle);
 rad2deg(separation_angle)
 beam_theta = deg2rad((0:1:120));
-beam_f = matlabFunction(beam_s); 
+beam_f = matlabFunction(beam_s);
 beam_profile_f = @(theta) beam_f(d1_epsilon0_f(separation_angle)/beta + epsilon0_f(separation_angle),...
     epsilon0_f(separation_angle), theta);
 beam_R = R -  beam_profile_f(beam_theta);
 legend beam terrain
-%% graphics objects denoted with _l 
+%% graphics objects denoted with _l
 figure
 hold on
 set(0 , "DefaultLineLineWidth", 3)
 set(0 , "DefaultAxesFontSize", 20)
 set(gca ,"fontsize", 30)
 tyre_l = line(R*cos(base_theta) , R*sin(base_theta) , "color" , "b", "linestyle", "--");
-terrain_l = line(Ot(1)+ rho*cos(base_theta), Ot(2) + rho*sin(base_theta), "color", "black"); 
+terrain_l = line(Ot(1)+ rho*cos(base_theta), Ot(2) + rho*sin(base_theta), "color", "black");
 rim_l = line(R_rim*cos(base_theta) , R_rim*sin(base_theta), "linestyle", "-.");
-tyre_centre_lable = text(0 , 0 , "O_{TCF}", "FontSize",20);
+contact_centre_l = line((R- penetration)*cos(theta0) , (R - penetration)*sin(theta0),...
+    "color", "black","marker", "*","markersize", 20);
+draw_coordinate_frame_polar(0 , 0 , [1 , 0] , 0.1*R, "TCF")
+draw_coordinate_frame_polar(Ot(1) , Ot(2) , [sin(theta0), cos(theta0)], 0.1*R , "CCF")
 separation_point = line(R_separation*cos(theta0 + separation_angle) , R_separation*sin(theta0+ separation_angle) , 'Marker',"*");
-beam_l = line(beam_R.*cos(theta0+ separation_angle + beam_theta) , beam_R.*sin(theta0+separation_angle+beam_theta), "linestyle", ":");
+beam_l = line(beam_R.*cos(theta0+ separation_angle + beam_theta) , beam_R.*sin(theta0+separation_angle+beam_theta),...
+    "linestyle", ":", "color", "red", "marker", ".", "markersize", 3);
 original_springs_l = [];
 % for t = 270:10:360
-%     original_springs_l(end+1) = line([tyre_l.XData(t), rim_l.XData(t)], [tyre_l.YData(t), rim_l.YData(t)]); 
+%     original_springs_l(end+1) = line([tyre_l.XData(t), rim_l.XData(t)], [tyre_l.YData(t), rim_l.YData(t)]);
 % end
 daspect([1, 1, 1])
+%%
+function draw_coordinate_frame_cartisian(centre_x, centre_y, x_direction, scale , name)
+x_direction = x_direction/(norm(x_direction));
+plot(centre_x , centre_y , "bla.", MarkerSize=20);
+quiver(centre_x , centre_y , x_direction(1) , x_direction(2),scale, "linewidth" , 3 , "color", "black","maxheadsize", 5);
+quiver(centre_x , centre_y , -x_direction(2) , x_direction(1) , scale, "linewidth" , 3, "color", "black", "maxheadsize", 5);
+text(centre_x - scale*x_direction(1) , centre_y - scale*x_direction(2), name);
+text(centre_x + 1.1*scale*x_direction(1) , centre_y + 1.1*scale*x_direction(2) , "X")
+text(centre_x - 1.1*scale*x_direction(2) , centre_y + 1.1*scale*x_direction(1) , "Y")
+end
+function draw_coordinate_frame_polar(centre_x , centre_y , x_direction, scale, name)
+x_direction = x_direction/(norm(x_direction));
+plot(centre_x , centre_y , "bla.", MarkerSize=20);
+quiver(centre_x , centre_y , x_direction(1) , x_direction(2),scale, "linewidth" , 3 , "color", "black","maxheadsize", 5);
+quiver(centre_x , centre_y , -x_direction(2) , x_direction(1) , scale, "linewidth" , 3, "color", "black", "maxheadsize", 5);
+text(centre_x - 2*scale*x_direction(1) , centre_y, name, FontSize=18);
+text(centre_x + 1.1*scale*x_direction(1) , centre_y + 1.1*scale*x_direction(2) , "\theta_0", FontSize=18)
+end
---
+++ scripts/symblic_math.m
@@ -0,0 +1,11 @@
+syms rho alpha gamma D A B beta phi real
+clc
+eps = exp(-beta*phi)*(A*sin(beta*phi) + B*cos(beta*phi));
+eps_d1 = diff(eps, phi);
+eps_d2 = diff(eps_d1 , phi);
+eps_exp  = eval(subs(eps_d2 , phi , 0));
+alpha = asin((D/rho)*sin(gamma))- gamma;
+Rp = rho*sin(alpha)/sin(gamma);
+d1 = diff(Rp , gamma);
+d2 = diff(d1 , gamma);
+d2_simp = subs(d2 , (gamma - asin(D*sin(gamma)/rho)), 'alpha');
